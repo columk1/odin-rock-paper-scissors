d9a3007102e11a3fa78531cb7200c1cf82ab35df
Colum Kelly <columk1@users.noreply.github.com>
2023-06-16 23:53:51 -0700

    fix: updated functions feat: added game function

--- script.js
+++ script.js
@@ -1,25 +1,70 @@
-const moves = {0: 'rock', 1: 'paper', 2: 'scissors'};
+/*
+A game of rock paper scissors playable in the console. 
+The Odin Project: https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
+*/
+
+// Declare object variable with the 3 handsign choices and a matrix for calculating the outcome of a game
+const moves = ['rock', 'paper', 'scissors'];
 const outcomes = [[0, -1, 1], [1, 0, -1], [-1, 1, 0]];
-const input = prompt("3...2...1...").toLowerCase();
 
-player = Object.keys(moves)[Object.values(moves).indexOf(input)];
-computer = getComputerChoice();
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let winner = "";
 
-function getComputerChoice() {
-    let randomIndex = Math.round((Math.random() * 10)) % 3;
-    return [randomIndex];
+    for(let i = 0; i < 5; i++) {
+        // Prompt user for their choice
+        const input = prompt("3...2...1...").toLowerCase();
+        // Assign an integer value based on input
+        player = input;
+        // Assign a random integer value
+        computer = moves[getRandom(3)];
+
+        result = playRound(player, computer);
+
+        // Update and print score
+        result == 1 ? playerScore++ : result == -1 ? computerScore++ : result;
+        console.log(playerScore + " : " + computerScore);
+    }
+    playerScore == computerScore ? winner = "It's a Draw!" : playerScore > computerScore ? winner = "You Win!" : winner = "Computer Wins!";
+    console.log(winner);
 }
 
+// Play round. Returns an integer result. 1, -1 or 0 (win, loss or draw)
 function playRound(player, computer) {
-    if(player && computer) {
-        return outcomes[player][computer];
+    if (moves.includes(player) && moves.includes(computer)) {
+        // Assign integer to indicate win, loss or draw based on the outcomes matrix
+        result = outcomes[moves.indexOf(player)][moves.indexOf(computer)];
+        // Assign string values of each player's move
+        returnStr = "";
+        switch(result) {
+            case -1:
+                returnStr = "You Lose! " + capitalizeFirstLetter(computer) + " beats " + capitalizeFirstLetter(player) + ".";
+                break;
+            case 1:
+                returnStr = "You Win! " + capitalizeFirstLetter(player) + " beats " + capitalizeFirstLetter(computer) + ".";
+                break;
+            case 0:
+                returnStr = "Draw! " + capitalizeFirstLetter(player) + " vs " + capitalizeFirstLetter(computer) + ".";
+                break;
+        }
+        console.log(returnStr);
+        return result;
     } else {
         throw new Error("Must choose 'rock', 'paper' or 'scissors'.");
     }
 }
 
-// Debugging
-console.log(moves[player]);
-console.log(moves[computer]);
-console.log(playRound(player, computer));
+function capitalizeFirstLetter(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+// Get a random number between 1 and the max parameter
+function getRandom(max) {
+    let randomIndex = Math.round((Math.random() * 10)) % max;
+    return [randomIndex];
+}
+
+game();
+
 
